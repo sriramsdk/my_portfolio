1258d26aa5d42b68bdd849caa9a2dc5fb9d27ca3
Sriram.R <146877320+sriramsdk@users.noreply.github.com>
2024-01-07 11:57:33 +0530

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ url('css/app.css') }}">
+    <link rel="stylesheet" href="{{ url('css/home.css') }}">
+    <title>Home Page</title>
+</head>
+<body>
+    <div class="container w-100 h-100">
+        <div class="container_1">
+            <h1>Home</h1>
+        </div>
+    </div>
+    
+</body>
+</html>
