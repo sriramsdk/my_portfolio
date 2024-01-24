f1c1031fa3a7baf16ff1d68fadab8a966a243740
Sriram.R <146877320+sriramsdk@users.noreply.github.com>
2024-01-24 23:00:45 +0530

    My first commit

---
+++ my_portfolio/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ url('css/app.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid ">
+        <div class="container-fluid w-100 h-100 mt-2 bg-primary rounded">
+        <h1>Home</h1>
+        </div>
+    </div>
+
+</body>
+</html> 
